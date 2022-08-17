20dc4c75341ba9d6480da6849759085f2a3a955a
Ye Jin <49353358+rosaliejin@users.noreply.github.com>
2022-08-17 14:36:38 -0700

    Update kmeans.py

--- ALL/kmeans.py
+++ ALL/kmeans.py
@@ -6,8 +6,6 @@ line = file.readline()
 data = []
 eachrow = []
 
-#c = open("B-ALL-center.txt", "a")
-#l = open("B-ALL-label.txt", "a")
 
 while line != "":
     line = line.replace("\n","")
@@ -22,18 +20,8 @@ while line != "":
 print(data)
 
 kmeans = KMeans(n_clusters=300, random_state=0, max_iter=300)
-#kmeans = KMeans(n_clusters=5, random_state=0, max_iter=20)
 kmeans.fit(data)
-print(centers)
 centers = kmeans.cluster_centers_
 labels = kmeans.labels_
 np.savetxt("BALL-large-300-center.txt",centers)
 np.savetxt("BALL-large-300-label.txt",labels)
-#print(centers)
-#print(labels)
-#a = np.array2string(centers,  separator=',')
-#b = np.array2string(labels,  separator=',')
-#c.write(a)
-#l.write(b)
-#c.close()
-#l.close()
